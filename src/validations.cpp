cb61f48decc4844b2e6c8c94b5109a0ac3703761
xmkabel <mkabel190@gmail.com>
2026-05-05 01:18:23 +0300

    Added getValidFloat function for non-negative float input validation

--- src/validations.cpp
+++ src/validations.cpp
@@ -5,7 +5,17 @@ int getValidInt(){
     while (!(cin >> value) || value < 0) {
         cin.clear();
         cin.ignore(1000, '\n');
-        cout << "Invalid input.Enter a valid non-negative number: ";
+        cout << "Invalid input. Enter a valid non-negative number: ";
+    }
+    return value;
+}
+
+float getValidFloat(){
+    float value;
+    while (!(cin >> value) || value < 0) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Invalid input. Enter a non-negative number: ";
     }
     return value;
 }
